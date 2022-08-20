b69f97564ed16f503f44c99cc67daac61728f363
Akhmad Maulana Akbar <akhmad0598@gmail.com>
2022-08-21 00:42:36 +0700

    fix expired and now time condition

--- main2.py
+++ main2.py
@@ -13,51 +13,67 @@ MACHINE_ID = "TEST01"
 
 class Main():
     def __init__(self):
-        self.lcd = LCD1602()
-        self.ultrasonic = Ultrasonic(echo_pin=27, trigger_pin=17)
+        # GateEntry()
+        # GateExit()
+
+class GateEntry():
+    def __init__(self):
+        lcd = LCD1602()
+        ultrasonic = Ultrasonic(echo_pin=27, trigger_pin=17)
         # NOTE: path to USB device when it's already plugged will be at /dev/input/event0
         # if it plugged after raspi up, it will be at /dev/input/event1
         device_path = "/dev/input/event0"
-        self.usb_scanner = USBScanner(device_path)
+        usb_scanner = USBScanner(device_path)
         # Gate take device_path too, but since it's only 1 device_path
         # i think it's ok to use the default one /dev/ttyAMA0
-        self.gate = Gate()
+        gate = Gate()
         Helper.log_print("Parking IoT start")
-        self.mqtt = Mqtt(host="172.104.182.166", port=1883, keepalive=60, username="testparking", password="123456")
-        self.db = DB("scanned.db")
+        mqtt = Mqtt(host="172.104.182.166", port=1883, keepalive=60, username="testparking", password="123456")
+        db = DB("scanned.db")
 
         Helper.log_print("initilaize gate")
-
-    def gate_entry(self):
-        Helper.log_print("Gate entry ready!")
         while True:
             # 1. print waiting for QR Scan message
-            self.lcd.waiting_message()
+            lcd.waiting_message()
             try:
-                scanned_string = self.usb_scanner.readQRCode()
+                scanned_string = usb_scanner.readQRCode()
                 scanned_string = base64.b64decode(scanned_string)
                 # take scanned string > check the command (checkin, checkout)
                 # if checkin, just directly open the gate, 
                 json_data = Helper.parse_json_qrcode(scanned_string)
                 if not json_data['parking_type'] == "checkin":
                     continue
-                self.db.insert_data({'parking_type': json_data['parking_type'], 'code': json_data['code']})
-                self.mqtt.publish_command(MACHINE_ID, json_data['parking_type'], json_data)
+                db.insert_data({'parking_type': json_data['parking_type'], 'code': json_data['code']})
+                mqtt.publish_command(MACHINE_ID, json_data['parking_type'], json_data)
                 # print message selamat datang
-                self.lcd.scan_success_message(gate=0)
+                lcd.scan_success_message(gate=0)
                 # open gate parking
                 Helper.log_print("checkin success")
-                self.gate.open_gate()
+                gate.open_gate()
 
                 # when vehicle passing the gate, loop to first step
-                if self.ultrasonic.is_vehicle_pass():
+                if ultrasonic.is_vehicle_pass():
                     Helper.log_print("vehicle passing the gate")
-                    self.gate.close_gate()
+                    gate.close_gate()
             except Exception as e:
                 Helper.log_print(e)
-    
-    def gate_exit(self):
-        Helper.log_print("Gate Exit ready!")
+
+class GateExit():
+    def __init__(self):
+        lcd = LCD1602()
+        ultrasonic = Ultrasonic(echo_pin=27, trigger_pin=17)
+        # NOTE: path to USB device when it's already plugged will be at /dev/input/event0
+        # if it plugged after raspi up, it will be at /dev/input/event1
+        device_path = "/dev/input/event0"
+        usb_scanner = USBScanner(device_path)
+        # Gate take device_path too, but since it's only 1 device_path
+        # i think it's ok to use the default one /dev/ttyAMA0
+        gate = Gate()
+        Helper.log_print("Parking IoT start")
+        mqtt = Mqtt(host="172.104.182.166", port=1883, keepalive=60, username="testparking", password="123456")
+        db = DB("scanned.db")
+
+        Helper.log_print("initilaize gate")
         while True:
             # 1. print waiting for QR Scan message
             self.lcd.waiting_message()
@@ -82,12 +98,8 @@ class Main():
                     continue
                 else:
                     expired_time = Helper.parse_datetime(json_data['expired'])
-                    expired_timestamp = Helper.parse_to_timestamp(date=expired_time)
-                    Helper.log_print(f"expired_time: {expired_timestamp}")
-                    now_timestamp = Helper.parse_to_timestamp()
-                    Helper.log_print(f"current_time: {now_timestamp}")
-                    # If expired_timestamp already passed by now_timestamp
-                    if expired_timestamp < now_timestamp:
+                    if Helper.parse_to_timestamp(date=expired_time) > Helper.parse_to_timestamp():
+                        # When expired time is more than current time, print expired message
                         self.lcd.expired_message()
                         Helper.log_print("expired")
                         time.sleep(5)
@@ -108,6 +120,3 @@ class Main():
 
 if __name__ == '__main__':
     main = Main()
-    # uncomment one of initialization below to choose between gate entry and exit
-    # main.gate_entry()
-    # main.gate_exit()
